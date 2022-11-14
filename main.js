2b01c60fb01ffb564d9632d82359fd84297c3436
bipulkumar254 <bipulkumar2540@yahoo.com>
2022-11-14 18:55:17 +0530

    count changed

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let elem = document.querySelectorAll(".active");
 let btn = document.querySelector(".read");
 
 let dot = document.querySelectorAll(".fa-circle");
+
+let count = document.querySelector(".notification__count");
 btn.addEventListener("click", () => {
   elem.forEach((item) => {
     item.classList.remove("active");
@@ -10,4 +12,5 @@ btn.addEventListener("click", () => {
   dot.forEach((item) => {
     item.classList.remove("fa-circle");
   });
+  count.innerHTML = "0";
 });
